f7233b9120c572e44793fedb35556c4858616ca6
skostadinov0141 <simeon.kostadinov0141@gmail.com>
2023-03-14 17:05:46 +0100

    finished discord auth for now

--- routers/discord_authentication.py
+++ routers/discord_authentication.py
@@ -37,6 +37,39 @@ router = APIRouter(
 
 
 # region API Methods
+
+def validate_token(req: Request):
+
+    # Get unparsed token and split it i authToken and discordId
+    unparsedToken = req.headers["Authorization"].split('%')
+    authToken = unparsedToken[0]
+    discordId = unparsedToken[1]
+
+    # Find expected entry in DB
+    entry = database.users.find_one({'discord_id':discordId})
+
+    # Return true if auth conditions are met
+    if entry is None:
+        raise HTTPException(status_code=404, detail={
+            'en':{
+                'description':'No such account.'
+            },
+            'de':{
+                'description':'Dieser Account existiert nicht.'
+            }
+        })
+    if entry['auth_token'] != authToken:
+        raise HTTPException(status_code=400, detail={
+            'en':{
+                'description':'The Auth-Token is not valid.'
+            },
+            'de':{
+                'description':'Das Auth-Token ist falsch.'
+            }
+        })
+    return True
+
+
 @router.post('/register_account')
 async def register_account(discord_id: str, discord_username: str):
 
@@ -72,6 +105,24 @@ async def register_account(discord_id: str, discord_username: str):
     return database.users.find_one({'auth_token': newEntry['auth_token']}, {'_id':False})
 
 
+@router.delete('/{discord_id}/delete_account')
+async def delete_account(discord_id: str):
+
+    # Get entry from DB, if empty raise error
+    entry = database.users.find_one({'discord_id': discord_id})
+    if entry is None:
+        raise HTTPException(status_code=404, detail={
+            'en':{
+                'description':'No such account.'
+            },
+            'de':{
+                'description':'Dieser Account existiert nicht.'
+            }
+        })
+    database.users.find_one_and_delete({'discord_id': discord_id})
+    return {'result':'Deleted successfully.'}
+
+
 @router.get('/{discord_id}/get_auth_token')
 async def get_auth_token(discord_id:str):
 
@@ -91,22 +142,27 @@ async def get_auth_token(discord_id:str):
     return f"{entry['auth_token']}%{entry['discord_id']}"
 
 
-def validate_token(req: Request):
-
-    # Get unparsed token and split it i authToken and discordId
-    unparsedToken = req.headers["Authorization"].split('%')
-    authToken = unparsedToken[0]
-    discordId = unparsedToken[1]
-
-    # Find expected entry in DB
-    entry = database.users.find_one({'discord_id':discordId})
-
-    # Return true if auth conditions are met
+@router.patch('/{discord_id}/regen_auth_token')
+async def regen_auth_token(discord_id:str):
+    # get entry, if empty return 404 error
+    entry = database.users.find_one({'discord_id': discord_id})
     if entry is None:
-        return False
-    if entry['auth_token'] != authToken:
-        return False
-    return True
-
-
+        raise HTTPException(status_code=404, detail={
+            'en':{
+                'description':'No such account.'
+            },
+            'de':{
+                'description':'Dieser Account existiert nicht.'
+            }
+        })
+    
+    # Generate new UUID, keep generating until no match in DB
+    entry['auth_token'] = str(uuid.uuid4())
+    while database.users.find_one({'auth_token':entry['auth_token']}) is not None:
+        entry['auth_token'] = str(uuid.uuid4())
+    
+    # Replace current auth_token with new unique one
+    database.users.find_one_and_update({'discord_id':discord_id},{'$set':{'auth_token':entry['auth_token']}})
+    return database.users.find_one({'discord_id': discord_id}, {'_id':False})
+    
 # endregion
