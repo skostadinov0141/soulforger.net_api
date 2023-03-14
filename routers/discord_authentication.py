eef48c9c0d2e57b5d0d62780c7cf389e57dc8916
skostadinov0141 <simeon.kostadinov0141@gmail.com>
2023-03-14 13:09:45 +0100

    added get_auth_token endpoint and token validation method

--- routers/discord_authentication.py
+++ routers/discord_authentication.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request, Depends
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from urllib.parse import quote_plus 
+import bson
 import yaml
 import uuid
 
@@ -37,16 +38,75 @@ router = APIRouter(
 
 # region API Methods
 @router.post('/register_account')
-async def register_account(discord_id: str):
+async def register_account(discord_id: str, discord_username: str):
+
     # attempt to find an existing entry, if found raise error
     databaseEntry = database.users.find_one({'discord_id' : discord_id})
     if databaseEntry is not None:
         raise HTTPException(status_code=409, detail={
-            'description' : 'Account already registered'
+            'en':{
+                'description' : 'Account already registered.'
+            },
+            'de':{
+                'description': 'Das Account wurde schon angemeldet.'
+            }
         })
+    
+    # Generate entry
     newEntry = {
-        'uuid':uuid.uuid4(),
-        'discord_id':discord_id
+        'auth_token':str(uuid.uuid4()),
+        'discord_id':discord_id,
+        'discord_username':discord_username,
+        'preferred_lang':'de',
+        'discord_guilds':[],
+        'active_sessions':[],
+        'characters':[]
     }
-    return newEntry
+
+    # ensure no collisions are present
+    while database.users.find_one({'auth_token':newEntry['auth_token']}) is not None:
+        newEntry['auth_token'] = str(uuid.uuid4())
+
+    # write to DB
+    database.users.insert_one(newEntry)
+    return database.users.find_one({'auth_token': newEntry['auth_token']}, {'_id':False})
+
+
+@router.get('/{discord_id}/get_auth_token')
+async def get_auth_token(discord_id:str):
+
+    # get entry, if empty return 404 error
+    entry = database.users.find_one({'discord_id': discord_id})
+    if entry is None:
+        raise HTTPException(status_code=404, detail={
+            'en':{
+                'description':'No such account.'
+            },
+            'de':{
+                'description':'Dieser Account existiert nicht.'
+            }
+        })
+    
+    # return auth_token
+    return f"{entry['auth_token']}%{entry['discord_id']}"
+
+
+def validate_token(req: Request):
+
+    # Get unparsed token and split it i authToken and discordId
+    unparsedToken = req.headers["Authorization"].split('%')
+    authToken = unparsedToken[0]
+    discordId = unparsedToken[1]
+
+    # Find expected entry in DB
+    entry = database.users.find_one({'discord_id':discordId})
+
+    # Return true if auth conditions are met
+    if entry is None:
+        return False
+    if entry['auth_token'] != authToken:
+        return False
+    return True
+
+
 # endregion
