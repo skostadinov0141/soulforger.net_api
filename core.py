f7453f2e654b2610c5be2552f28ca5d38f841a74
skostadinov0141 <simeon.kostadinov0141@gmail.com>
2023-03-27 02:45:03 +0200

    added talent checks endpoint

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routers import discord_authentication
 from routers import character_creation
+from routers import skill_checks
 
 app = FastAPI()
 
@@ -16,3 +17,4 @@ app.add_middleware(
 
 app.include_router(discord_authentication.router)
 app.include_router(character_creation.router)
+app.include_router(skill_checks.router)
