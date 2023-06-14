bff425000d6729c108abdf5f0544d3a8a60ddef4
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2023-06-14 02:50:36 +0200

    changed register to reflect frontend validation

--- routers/account_management.py
+++ routers/account_management.py
@@ -55,75 +55,32 @@ async def validate_session( request: Request, user_id: ObjectId = Depends(authen
 
 @router.post('/register')
 async def register_account(acc: Account):
-    # Validate email and password, store the results in a list of dicts
-    validations = [
-        validate_email(acc.email),
-        validate_pw(acc.password)
-    ]
-    # iterate over the validation dicts and if any validation is false return a 400 Bad Request
-    # additionally make sure that the eula has been accepted
-    final_result = True
-    final_details = []
-    for i in validations:
-        if i['result'] == False:
-            final_result = False
-        for d in i['details']:
-            final_details.append(d)
-    if acc.eula == False:
-        final_result = False
-        final_details.append({
-            'category':'eula',
-            'detail':'NONE'
-        })
-    if acc.password != acc.password_confirmation:
-        final_result = False
-        final_details.append({
-            'category':'password_confirmation',
-            'detail':'Die Passwörter stimmen nicht überein.'
-        })
-    if acc.display_name == '':
-        final_result = False
-        final_details.append({
-            'category':'display_name',
-            'detail':'Der Anzeigename darf nicht leer sein.'
-        })
-    if len(acc.display_name) < 5:
-        final_result = False
-        final_details.append({
-            'category':'display_name',
-            'detail':'Der Anzeigename ist zu kurz. (min. 5)'
-        })
-    if len(acc.display_name) > 20:
-        final_result = False
-        final_details.append({
-            'category':'display_name',
-            'detail':'Der Anzeigename ist zu lang. (max. 20)'
-        })
-    if final_result == False:
-        raise HTTPException(400,final_details)
-    # Hash pw and create an account in the DB if all inputs are valid
-    hashedPWD = bcrypt.hashpw(acc.password.encode(), bcrypt.gensalt(rounds=14))
-    user_account = {
-        'email':acc.email,
-        'password_hash':hashedPWD.decode(),
-        'characters_list':[],
-        'campaigns_list':[],
-        'games_list':[],
-        'community_contributions_list':[],
-    }
-    # Create needed collections and reference them
-    profile : Profile = Profile(
-        display_name=acc.display_name
-    )
-    # insert into database
-    related_dicts = db.general.createRelationOO(dict1=user_account,rel_name1='profile_document',dict2=profile.dict(),rel_name2='owner')
-    db.general.getCollection('users','am').insert_one(related_dicts[0])
-    db.general.getCollection('profiles').insert_one(related_dicts[1])
-    # compile return dict
-    return_dict = db.profiles.getProfileFromUser(related_dicts[0]['_id'])
-    del return_dict['owner']
-    del return_dict['_id']
-    return return_dict
+    # Check if email is already in use
+    if db.accounts.checkIfEmailExist(acc.email):
+        hashedPWD = bcrypt.hashpw(acc.password.encode(), bcrypt.gensalt(rounds=14))
+        user_account = {
+            'email':acc.email,
+            'password_hash':hashedPWD.decode(),
+            'characters_list':[],
+            'campaigns_list':[],
+            'games_list':[],
+            'community_contributions_list':[],
+        }
+        # Create needed collections and reference them
+        profile : Profile = Profile(
+            display_name=acc.display_name
+        )
+        # insert into database
+        related_dicts = db.general.createRelationOO(dict1=user_account,rel_name1='profile_document',dict2=profile.dict(),rel_name2='owner')
+        db.general.getCollection('users','am').insert_one(related_dicts[0])
+        db.general.getCollection('profiles').insert_one(related_dicts[1])
+        # compile return dict
+        return_dict = db.profiles.getProfileFromUser(related_dicts[0]['_id'])
+        del return_dict['owner']
+        del return_dict['_id']
+        return return_dict
+    else:
+        raise HTTPException(status_code=400, detail='E-Mail bereits in Verwendung.')
 
 
 @router.post("/login")
