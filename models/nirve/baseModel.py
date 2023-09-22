cad446184595f42dc1eb13a931c1fa2fb780be73
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2023-09-22 12:32:38 +0200

    added location to nirve base_model

--- models/nirve/baseModel.py
+++ models/nirve/baseModel.py
@@ -3,3 +3,6 @@ from pydantic import BaseModel
 class NirveBaseModel(BaseModel):
     name: str
     description: str
+    # . = current field in the model
+    # > = the id of a list item
+    location: str
