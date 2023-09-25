6ad6d0a345b18120b6a207ad9cc421f85a74548b
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2023-09-25 23:04:49 +0200

    added new packages to venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\simeo\Documents\Programming\dsa.soulforger.net\api\.venv
+set VIRTUAL_ENV=c:\Users\pands\Documents\Programming\dsa.soulforger.net\api\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
