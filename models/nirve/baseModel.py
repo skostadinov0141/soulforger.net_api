ccebcb31135b1b4dac89ee3b59011d0256e33913
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2023-10-24 17:29:37 +0200

    created nest skeleton

--- models/nirve/baseModel.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class NirveBaseModel(BaseModel):
-    name: str
-    description: str
-    # . = current field in the model
-    # > = the id of a list item
-    location: str
